6e0cd901e0566cb772e458c49cd1da721adba96a
Justin <j.lum07@gmail.com>
2018-04-26 17:10:20 -0400

    removed commented code

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -13,30 +13,13 @@ module.exports = function makeDataHelpers(db) {
       db.collection('tweets').insertOne(newTweet);
       callback(null, true);
 
-      // db.close();
-
-      // simulateDelay(() => {
-      //   db.tweets.push(newTweet);
-      //   callback(null, true);
-      // });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
 
-
-
       db.collection("tweets").find().sort({created_at: 1}).toArray(callback);
 
-      // db.close();
-
-      // callback(null, db.collection("tweets").find().sort({created_at: 1}).toArray());
-
-
-      // simulateDelay(() => {
-      //   const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-      //   callback(null, db.tweets.sort(sortNewestFirst));
-      // });
     }
 
   };
