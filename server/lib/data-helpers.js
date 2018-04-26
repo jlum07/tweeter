d94282c2224ecde3830a767292d00460e1c2be87
Justin Lum <j.lum07@gmail.com>
2018-04-26 13:08:40 +0000

    added mongo db

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,18 +9,38 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+
+      db.collection('tweets').insertOne(newTweet);
+      callback(null, true);
+
+      // db.close();
+
+      // simulateDelay(() => {
+      //   db.tweets.push(newTweet);
+      //   callback(null, true);
+      // });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+
+
+
+      db.collection("tweets").find().sort({created_at: 1}).toArray(callback);
+
+      // db.close();
+
+      // callback(null, db.collection("tweets").find().sort({created_at: 1}).toArray());
+
+      //
+      // db.close();
+      // const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      // callback(null, db.tweets.sort(sortNewestFirst));
+
+      // simulateDelay(() => {
+      //   const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      //   callback(null, db.tweets.sort(sortNewestFirst));
+      // });
     }
 
   };
