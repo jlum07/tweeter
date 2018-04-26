5a76c8207e9c408bf2895870d954bca9ad9d0be8
Justin <j.lum07@gmail.com>
2018-04-26 16:18:13 -0400

    added bling to my vars

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,23 +3,17 @@ $(document).ready(function() {
   $("section#new-tweet form textarea").on("input", function() {
     // input propertychange paste -- would we need them all?
 
-    // console.log($(this).val().length);
-    var chars = $(this).val().length;
-    var count = $(this).parent().find('.counter');
+    var $chars = $(this).val().length;
+    var $count = $(this).parent().find('.counter');
 
-    count.text(140 - chars);
+    $count.text(140 - $chars);
 
-    // $(this).parent().find('.counter').text(140 - $(this).val().length);
-
-    if (chars > 140) {
-      // change class or id instead of css which is technically inline
+    if ($chars > 140) {
       // $(this).parent().find('.counter').css("color", "red");
-      count.addClass('count-red').removeClass('count-black');
-
-
+      $count.addClass('count-red').removeClass('count-black');
     } else {
       // $(this).parent().find('.counter').css("color", "black");
-      count.addClass('count-black').removeClass('count-red');
+      $count.addClass('count-black').removeClass('count-red');
     }
 
   });
