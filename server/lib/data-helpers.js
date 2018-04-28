4cbae2b61440f69888863ab28fa111d47c5f91d5
Justin <j.lum07@gmail.com>
2018-04-27 20:36:06 -0400

    Added ability to query a single tweet by _id

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -2,6 +2,7 @@
 
 // Simulates the kind of delay we see with network or filesystem operations
 const simulateDelay = require("./util/simulate-delay");
+const {ObjectId} = require('mongodb');
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
@@ -19,6 +20,25 @@ module.exports = function makeDataHelpers(db) {
     getTweets: function(callback) {
 
       db.collection("tweets").find().sort({created_at: 1}).toArray(callback);
+
+    },
+
+    // Return only one tweet to display
+    getOneTweet: function(id ,callback) {
+
+      // console.log("getonetweet")
+      // db.collection("tweets").find({"_id" : req.params.id}).toArray(callback);
+      // db.collection("tweets").find({"_id" : "5ae12cf16245e23760cf6655"});
+      // callback(null, true);
+
+      // db.collection("tweets").findOne({"_id" : "5ae12cf16245e23760cf6655"}, callback);
+
+      // console.log(id);
+      // db.collection("tweets").findOne(ObjectId(`5ae12cf16245e23760cf6655`), callback);
+
+      db.collection("tweets").findOne(ObjectId(id), callback);
+
+      // callback(null, db.collection("tweets").find({"_id" : "5ae12cf16245e23760cf6655"}));
 
     }
 
